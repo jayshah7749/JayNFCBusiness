c9908cccc539768df2660cb17ef7bc52806b9401
Viraj Patel <virajpatel325@gmail.com>
2023-01-25 23:03:56 -0500

    added the ability to check if the broswer is mobile or desktop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { isMobile } from 'mobile-device-detect';
 
+// <img src="https://picsum.photos/200/300" className="nue-button" alt=""></img>
 function App() {
-  return (
-    <img src="https://picsum.photos/200/300" className="nue-button" alt=""></img>
-  );
+    return (
+        <div>
+            {
+                isMobile ?
+                    <div>
+                        <img src="https://picsum.photos/200/300" className="nue-profile" alt=""></img>
+                        <div className="nue-bio"></div>
+                        <div className="nue-button"></div>
+                        <div className="nue-button"></div>
+                        <div className="nue-button"></div>
+                    </div> :
+                    <div></div>
+            }
+        </div>
+    )
 }
 
 export default App;
