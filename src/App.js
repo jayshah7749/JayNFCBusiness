8aa747f9c15fd3ab765e459c73fb0db7d8cb5683
Viraj Patel <virajpatel325@gmail.com>
2023-01-30 06:03:01 -0500

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -52,34 +52,34 @@ function App() {
                             <p>Hey there, it's <b><span style={{ color: "purple" }}>Viraj</span></b>! I currently work at <b><span style={{ color: "purple" }}>JPMorgan Chase & Co. as a Software Engineer</span></b>. This is my nfc profile page, feel free to click any of those buttons to <b><span style={{ color: "purple" }}>connect with me</span></b> via that network.</p>
                         </div>
                         <div className="center-horizontal-div">
-                            <div id="phone-btn" className="nue-button" data-clipboard-text="6146578396">
+                            <div id="phone-btn" className="nue-button" data-clipboard-text="6146578396" onClick={() => { goToURL('phone') }}>
                                 <img className="img-icon" src="https://user-images.githubusercontent.com/37918393/215451245-b16f1c3c-bab4-49b6-ae84-e6eb1aece50c.png" alt=""/>
                             </div>
-                            <div className="nue-button">
+                            <div className="nue-button" onClick={() => { goToURL('link') }}>
                                 <img className="img-icon" src="https://user-images.githubusercontent.com/37918393/215445843-427ba3b4-20eb-4fa1-990d-a3a445f028f6.png" alt=""/>
                             </div>
                         </div>
                         <div className="center-horizontal-div">
-                            <div className="nue-button">
+                            <div className="nue-button" onClick={() => { goToURL('github') }}>
                                 <img className="img-icon" src="https://user-images.githubusercontent.com/37918393/215451239-40337cff-e27e-4d43-b4c5-43a2b111da69.png" alt=""/>
                             </div>
-                            <div className="nue-button">
+                            <div className="nue-button" onClick={() => { goToURL('linkedin') }}>
                                 <img className="img-icon" src="https://user-images.githubusercontent.com/37918393/215451249-b3842dc7-3f96-4fd7-9917-2d6f7aad6065.png" alt=""/>
                             </div>
                         </div>
                         <div className="center-horizontal-div">
-                            <div className="nue-button">
+                            <div className="nue-button" onClick={() => { goToURL('facebook') }}>
                                 <img className="img-icon" src="https://user-images.githubusercontent.com/37918393/215451247-e1e804d5-7128-4628-b739-1117552e1f99.png" alt=""/>
                             </div>
-                            <div className="nue-button">
+                            <div className="nue-button" onClick={() => { goToURL('instagram') }}>
                                 <img className="img-icon" src="https://user-images.githubusercontent.com/37918393/215451243-495feae0-e3cf-4286-ae67-17cf2d20230f.png" alt=""/>
                             </div>
                         </div>
                         <div className="center-horizontal-div">
-                            <div className="nue-button">
+                            <div className="nue-button" onClick={() => { goToURL('apple') }}>
                                 <img className="img-icon" src="https://user-images.githubusercontent.com/37918393/215451234-050607bc-846f-4d6b-b899-c78a034c1c02.png" alt=""/>
                             </div>
-                            <div className="nue-button">
+                            <div className="nue-button" onClick={() => { goToURL('android') }}>
                                 <img className="img-icon" src="https://user-images.githubusercontent.com/37918393/215451235-3c63e708-6a0d-4053-8d4a-081443249838.png" alt=""/>
                             </div>
                         </div>
